a46b4b2fff23e78fdf5813d95c9a295503a6a6f8
Jim Thoenen <jthoenen@gmail.com>
2016-01-17 15:46:31 -0600

    Timing instrumentation flasher..  see notes
    At the time of this commit creation, it pretty consistently shows 1 msec
    timing for keyboard.Process().

--- Boards/IB1/main.cpp
+++ Boards/IB1/main.cpp
@@ -15,19 +15,96 @@ namespace
 class Flasher
 {
     private:
-        unsigned long m_nextTime;
+
+        enum StateEnum
+        {
+            IDLE,
+            FLASH_ON,
+            FLASH_OFF,
+            WAITING
+        };
+
+        unsigned long   m_nextTime;
+        uint8_t         m_pulseCount;
+        StateEnum       m_state;
 
     public:
-        Flasher(): m_nextTime(0) { }
+        Flasher(): m_nextTime(0),
+                   m_pulseCount(0),
+                   m_state(IDLE) { }
 
-        void Process()
+        void SetPulseCount(uint8_t i_count)
         {
-            unsigned long now = Platform::GetMsec();
+            if (m_state == IDLE)
+            {
+                m_pulseCount = i_count;
+            }
+        }
+
+        void StrobeAndSetState(StateEnum i_state)
+        {
+            m_state = i_state;
+            PIND |= (1 << 6);
+            m_nextTime += 100;
+        }
+
+        void Process(unsigned long now)
+        {
+            switch (m_state)
+            {
+                case IDLE:
+
+                    if (m_pulseCount > 0)
+                    {
+                        m_nextTime = now;
+                        StrobeAndSetState(FLASH_ON);
+                    }
+                    break;
+
+                case FLASH_ON:
+
+                    if (m_nextTime < now)
+                    {
+                        StrobeAndSetState(FLASH_OFF);
+                    }
+                    break;
+
+                case FLASH_OFF:
+
+                    if (m_nextTime < now)
+                    {
+                        --m_pulseCount;
+                        if (m_pulseCount == 0)
+                        {
+                            m_state     = WAITING;
+                            m_nextTime += 1000;
+                        }
+                        else
+                        {
+                            StrobeAndSetState(FLASH_ON);
+                        }
+                    }
+                    break;
+
+                case WAITING:
+
+                    if (m_nextTime < now)
+                    {
+                        m_state = IDLE;
+                    }
+                    break;
+
+                default:
+                    m_state = IDLE;
+                    break;
+            }
+/*
             if (now > m_nextTime)
             {
                 m_nextTime += 500;
                 PIND |= (1 << 6);
             }
+*/
         }
 };
 
@@ -39,10 +116,16 @@ int main (void)
     Platform::DelayMicrosecs<1000>();
 
     Flasher flash;
+    unsigned long startTime;
+    unsigned long endTime;
     while(1)
     {
+        startTime = Platform::GetMsec();
         keyboard.Process();
-        flash.Process();
+        endTime = Platform::GetMsec();
+
+        flash.SetPulseCount(static_cast<uint8_t>(endTime - startTime));
+        flash.Process(endTime);
     }
 
     return 0;
