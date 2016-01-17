6558945ddeefb55fda6cb6a6d395f6c90d9f9f43
Jim Thoenen <jthoenen@gmail.com>
2016-01-17 15:52:46 -0600

    Make test board use abbreviated key object type names

--- Boards/TestBoardAvr/TestBoard.h
+++ Boards/TestBoardAvr/TestBoard.h
@@ -8,11 +8,11 @@
 
 #include "Row.h"
 
-typedef boost::fusion::vector<KeyA,     KeyE, KeyE,     KeyNONE,            KeyA> Row0;
-typedef boost::fusion::vector<KeyB,     KeyF, KeyF,     KeyMOD_LEFT_SHIFT,  KeyB> Row1;
-typedef boost::fusion::vector<KeyC,     KeyG, KeyNONE,  KeyNONE,            KeyC> Row2;
-typedef boost::fusion::vector<KeyD,     KeyH, KeyNONE,  KeyH,               KeyD> Row3;
-typedef boost::fusion::vector<KeyNONE,  KeyI, KeyNONE,  KeyI,               KeyE> Row4;
+typedef boost::fusion::vector<KeyA,     KeyE, KeyE,     KeyNONE,   KeyA> Row0;
+typedef boost::fusion::vector<KeyB,     KeyF, KeyF,     KeyLSHIFT, KeyB> Row1;
+typedef boost::fusion::vector<KeyC,     KeyG, KeyNONE,  KeyNONE,   KeyC> Row2;
+typedef boost::fusion::vector<KeyD,     KeyH, KeyNONE,  KeyH,      KeyD> Row3;
+typedef boost::fusion::vector<KeyNONE,  KeyI, KeyNONE,  KeyI,      KeyE> Row4;
 
 typedef boost::fusion::vector<Row0, Row1, Row2, Row3, Row4> Matrix;
 
