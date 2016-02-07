edf2bac9dd37300582750cdf58e057b7c1cb5c9f
Jim Thoenen <jthoenen@gmail.com>
2016-02-07 07:52:33 -0600

    Fix keys being incorrectly reported as held down while in bootloader.

--- Core/BoardController.h
+++ Core/BoardController.h
@@ -12,19 +12,26 @@ class BoardController
 {
     private:
 
-        uint8_t m_modifiers;
+        uint8_t       m_modifiers;                  ///< USB modifier report byte
+        bool          m_isRebootingToBootloader;    ///< Indicates we're going to the bootloader shortly.
+        unsigned long m_bootLoaderJumpTime;         ///< The time in msec that we'll make the actual jump.
 
         // TODO: NKRO.  Until then, keep things really simple...
 
-        // Only track up to 10 keys because really, how often do people have more than 10 keys pressed at a time?
+        /// USB report array.
+        /// Only track up to 10 keys because really, how often do people have more than 10 keys pressed at a time?
+        /// Also, I don't know how I want to deal with NKRO yet.
         FixedArray<uint8_t, 10, 0> m_keyReportArray;
 
+        /// Key truth state.
         FixedArray<uint8_t, 256, 0> m_activeKeyTable;
 
     public:
 
         BoardController() :
-            m_modifiers (0)
+            m_modifiers               (0),
+            m_isRebootingToBootloader (false),
+            m_bootLoaderJumpTime      (0)
         {
         }
 
@@ -61,12 +68,31 @@ class BoardController
 
             // TODO: Carve features off into a generic fusion collection defined in board-specific area.
 
+            // Four-finger salute to put the keyboard into the bootloader.  Not concerned about latching this.
+            // Once all four keys are down, the keyboard reports all keys cleared/reset, and the reboot happens
+            // some amount time after the keys are physically released, much like a proper reset button's behavior.
             if (m_activeKeyTable[KEY_LEFT_CTRL]  and
                 m_activeKeyTable[KEY_LEFT_SHIFT] and
                 m_activeKeyTable[KEY_TAB]        and
                 m_activeKeyTable[KEY_BACKSPACE])
             {
-                Platform::JumpToBootloader();
+                // Don't jump immediately.  Allow a second to pass so that we can reset the key reports.
+                // Otherwise the OS thinks the board is just holding down the keys when really it's in the
+                // bootloader.
+                m_isRebootingToBootloader = true;
+                m_bootLoaderJumpTime      = Platform::GetMsec() + 1000;
+            }
+
+            if (m_isRebootingToBootloader)
+            {
+                m_keyReportArray.clear();
+                m_modifiers = 0;
+
+                if (Platform::GetMsec() > m_bootLoaderJumpTime)
+                {
+                    // No return!
+                    Platform::JumpToBootloader();
+                }
             }
 
             UsbInterface::Process(m_keyReportArray.Get(),
