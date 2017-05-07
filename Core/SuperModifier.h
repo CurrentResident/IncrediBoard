923f13fa3e3f7d2c4c24f1949225c690d97d3d2a
Jim Thoenen <jthoenen@gmail.com>
2017-05-07 18:16:24 -0500

    Move PressKeys functor to SuperModifier.  Code size drop by 158!

--- Core/SuperModifier.h
+++ Core/SuperModifier.h
@@ -8,134 +8,21 @@
 #include "Key.h"
 #include "Platform.h"
 
-#if 0
-//template <typename CollectionType>
-/*
-template <typename CollectionType>
-struct PressKeysB
-{
-    typedef typename boost::fusion::result_of::begin<CollectionType>::type BeginType;
-    typedef typename boost::fusion::result_of::end<CollectionType>::type EndType;
-
-    template <uint8_t T_KEY_CODE>
-    void Press(Key<T_KEY_CODE>)
-    {
-
-    }
-
-    template <typename I>
-    void operator() () const
-    {
-        operator()<typename boost::fusion::result_of::next<I>::type>();
-    }
-
-    template <typename I>
-    void operator() () const
-    {
-        operator()<typename boost::fusion::result_of::next<I>::type>();
-    }
-};
-*/
-
-template <typename CollectionType, typename Iterator>
-struct PressKeysA
-{
-
-    /*
-    template <typename KeyType>
-    void Press(BoardState& io_state) const
-    {
-    }
-    */
-
-    template <uint8_t T_KEY_CODE>
-    void Press(BoardState& io_state, Key<T_KEY_CODE>) const
-    {
-        io_state.ChangeKeyState<T_KEY_CODE>(1);
-    }
-
-    void operator() (BoardState& io_state) const
-    {
-        Press(io_state, typename boost::fusion::result_of::value_of<Iterator>::type());
-
-        //PressKeysA<CollectionType, typename boost::fusion::result_of::deref<Iterator>::type> b;
-        //b(io_state);
-
-        PressKeysA<CollectionType, typename boost::fusion::result_of::next<Iterator>::type> a;
-        a(io_state);
-    }
-};
-
-/*
-template <typename CollectionType, uint8_t T_KEY_CODE>
-struct PressKeysA<CollectionType, Key<T_KEY_CODE> >
-{
-    void operator() (BoardState& io_state) const
-    {
-    }
-};
-*/
-template <typename CollectionType>
-struct PressKeysA<CollectionType, typename boost::fusion::result_of::end<CollectionType>::type>
-{
-    void operator() (BoardState& io_state) const
-    {
-    }
-};
-
-template <typename CollectionType>
-struct PressKeys
-{
-    void operator() (BoardState& io_state) const
-    {
-        PressKeysA<CollectionType, typename boost::fusion::result_of::begin<CollectionType>::type> a;
-        a(io_state);
-    }
-};
-#endif
-
-struct PressKeys
-{
-    BoardState& state;
-    PressKeys(BoardState& io_state) : state(io_state) {}
-
-    template <uint8_t T_KEY_CODE>
-    void operator()(Key<T_KEY_CODE>) const
-    {
-        state.ChangeKeyState<T_KEY_CODE>(1);
-    }
-};
-/*
-namespace Press
-{
-    template <typename CollectionType, typename Iterator>
-    void One()
-        {
-            One<CollectionType, typename boost::fusion::result_of::next<Iterator>::type>();
-        }
-                //boost::fusion::result_of::begin
-    template <typename CollectionType>
-    void One<typename boost::fusion::result_of::end<CollectionType>::type>()
-        {
-            //One<CollectionType, typename boost::fusion::result_of::next<Iterator>::type>();
-        }
-
-    template <typename CollectionType>
-    void All()
-        {
-            One<CollectionType, typename boost::fusion::result_of::begin<CollectionType>::type>();
-        }
-};
-
-template <typename CollectionType>
-static void Press<CollectionType>::One<typename boost::fusion::result_of::end<CollectionType>::type>()
-    {
-    }
-
-*/
 template <typename ModifierKeyType, typename OtherKeysCollectionType>
 class SuperModifier
 {
+
+    struct PressKeys
+    {
+        BoardState& state;
+        PressKeys(BoardState& io_state) : state(io_state) {}
+
+        template <uint8_t T_KEY_CODE>
+        void operator()(Key<T_KEY_CODE>) const
+        {
+            state.ChangeKeyState<T_KEY_CODE>(1);
+        }
+    };
 
     public:
 
