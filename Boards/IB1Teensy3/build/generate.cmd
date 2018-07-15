12799e4cad3ea8296f80ab734bc732182c4a6bef
Jim Thoenen <jthoenen@gmail.com>
2018-07-15 16:03:34 -0500

    WIP fix for build-time regeneratation not finding the right compiler

--- Boards/IB1Teensy3/build/generate.cmd
+++ Boards/IB1Teensy3/build/generate.cmd
@@ -1,12 +1,5 @@
 @echo off
 
-:: Only needed if you're not providing your own compiler.
-::
-::set ARDUINOPATH=D:/arduino-1.6.13
-
-set TEENSYDUINOPATH=../../../libs/teensyduino
-set COMPILERPATH=D:/gcc-arm-none-eabi-7-2017-q4-major-win32/bin
-
 :: Shamelessly lifted from https://stackoverflow.com/questions/4781772/how-to-test-if-an-executable-exists-in-the-path-from-a-windows-batch-file
 ::
 for %%X in (sh.exe)    do (set FOUND_SH=%%~$PATH:X)
