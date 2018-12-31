a4469aa1a5f11468122db5eea1f3fc6c4b95ffe7
Jim Thoenen <jthoenen@gmail.com>
2018-12-30 18:43:21 -0600

    Some whitespace cleanup

--- Boards/IB4/PlatformBoardSpecific.h
+++ Boards/IB4/PlatformBoardSpecific.h
@@ -52,20 +52,20 @@ namespace Platform
     //
     template <> inline void ReadInputs<uint8_t, 17>(uint8_t (& o_inputs)[17])
     {
-        o_inputs[0]  = (digitalReadFast(0) == 0);
-        o_inputs[1]  = (digitalReadFast(1) == 0);
-        o_inputs[2]  = (digitalReadFast(2) == 0);
-        o_inputs[3]  = (digitalReadFast(3) == 0);
-        o_inputs[4]  = (digitalReadFast(4) == 0);
-        o_inputs[5]  = (digitalReadFast(5) == 0);
-        o_inputs[6]  = (digitalReadFast(6) == 0);
-        o_inputs[7]  = (digitalReadFast(7) == 0);
+        o_inputs[0]  = (digitalReadFast( 0) == 0);
+        o_inputs[1]  = (digitalReadFast( 1) == 0);
+        o_inputs[2]  = (digitalReadFast( 2) == 0);
+        o_inputs[3]  = (digitalReadFast( 3) == 0);
+        o_inputs[4]  = (digitalReadFast( 4) == 0);
+        o_inputs[5]  = (digitalReadFast( 5) == 0);
+        o_inputs[6]  = (digitalReadFast( 6) == 0);
+        o_inputs[7]  = (digitalReadFast( 7) == 0);
         o_inputs[8]  = (digitalReadFast(23) == 0);
         o_inputs[9]  = (digitalReadFast(22) == 0);
         o_inputs[10] = (digitalReadFast(21) == 0);
         o_inputs[11] = (digitalReadFast(20) == 0);
-        o_inputs[12] = (digitalReadFast(8) == 0);
-        o_inputs[13] = (digitalReadFast(9) == 0);
+        o_inputs[12] = (digitalReadFast( 8) == 0);
+        o_inputs[13] = (digitalReadFast( 9) == 0);
         o_inputs[14] = (digitalReadFast(19) == 0);
         o_inputs[15] = (digitalReadFast(18) == 0);
         o_inputs[16] = (digitalReadFast(17) == 0);
