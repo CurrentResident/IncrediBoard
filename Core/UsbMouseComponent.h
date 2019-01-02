974a6d0894442a6c607f1ac4e22daa3243717087
Jim Thoenen <jthoenen@gmail.com>
2019-01-02 15:23:26 -0600

    Use lookup table for direction command to unit vector

--- Core/UsbMouseComponent.h
+++ Core/UsbMouseComponent.h
@@ -1,6 +1,7 @@
 #ifndef USB_MOUSE_COMPONENT_H_
 #define USB_MOUSE_COMPONENT_H_
 
+#include <cmath>
 #include <stdint.h>
 
 #include "BoardState.h"
@@ -15,9 +16,25 @@ class UsbMouseComponent
     public:
 
         UsbMouseComponent() :
-            m_lastUpdateTime(Platform::GetMsec()),
-            m_mouseMode     (MOUSE_OFF)
+            m_lastUpdateTime  (Platform::GetMsec()),
+            m_mouseUnitVectors{ { { std::cos(M_PI * 0.75), std::sin(M_PI * 0.75) }, {  0l,  1l }, { std::cos(M_PI * 0.25), std::sin(M_PI * 0.25) } },
+                                { { -1l,  0l }, { 0l,  0l }, {  1l,  0l } },
+                                { { std::cos(M_PI * 1.25), std::sin(M_PI * 1.25) }, {  0l, -1l }, { std::cos(M_PI * 1.75), std::sin(M_PI * 1.75) } } },
+            m_mouseMode       (MOUSE_OFF)
         {
+            //m_mouseUnitVectors = ;
+
+                /*
+            m_mouseUnitVectors[DEG_0]   = {  1l,  0l };
+            m_mouseUnitVectors[DEG_90]  = {  0l,  1l };
+            m_mouseUnitVectors[DEG_180] = { -1l,  0l };
+            m_mouseUnitVectors[DEG_270] = {  0l, -1l };
+
+            m_mouseUnitVectors[DEG_45]  = { std::cos(M_PI * 0.25), std::sin(M_PI * 0.25) };
+            m_mouseUnitVectors[DEG_135] = { std::cos(M_PI * 0.75), std::sin(M_PI * 0.75) };
+            m_mouseUnitVectors[DEG_225] = { std::cos(M_PI * 1.25), std::sin(M_PI * 1.25) };
+            m_mouseUnitVectors[DEG_315] = { std::cos(M_PI * 1.75), std::sin(M_PI * 1.75) };
+            */
         }
 
         void MouseMotion(bool i_upIsActive,
@@ -25,20 +42,26 @@ class UsbMouseComponent
                          bool i_leftIsActive,
                          bool i_rightIsActive)
         {
-            const unsigned long now       = Platform::GetMsec();
-            const SignedFixedType deltaMsec = now - m_lastUpdateTime;
+            const unsigned long   now            = Platform::GetMsec();
+            const SignedFixedType deltaMsec      = now - m_lastUpdateTime;
+            const SignedFixedType deltaTSeconds  = deltaMsec / 1000l;
 
-            const SignedFixedType deltaTSeconds  = deltaMsec / 1000;
-
-            const SignedFixedType MAX_VELOCITY = 1000;          // In units per second.
+            const SignedFixedType MAX_VELOCITY = 1000l;          // In units per second.
 
             const SignedFixedType actualVelocity = MAX_VELOCITY * deltaTSeconds;
 
-            m_mouseState.velocity.x  = i_rightIsActive ? actualVelocity : 0;
-            m_mouseState.velocity.x -= i_leftIsActive  ? actualVelocity : 0;
+            const int row = 1 + (i_upIsActive * 1) + (i_downIsActive * -1);
+            const int col = 1 + (i_leftIsActive * -1) + (i_rightIsActive * 1);
 
-            m_mouseState.velocity.y  = i_downIsActive ? actualVelocity : 0;
-            m_mouseState.velocity.y -= i_upIsActive   ? actualVelocity : 0;
+            m_mouseState.velocity = m_mouseUnitVectors[row][col];
+
+            /*
+            m_mouseState.velocity.x  = i_rightIsActive ? actualVelocity : 0l;
+            m_mouseState.velocity.x -= i_leftIsActive  ? actualVelocity : 0l;
+
+            m_mouseState.velocity.y  = i_downIsActive ? actualVelocity : 0l;
+            m_mouseState.velocity.y -= i_upIsActive   ? actualVelocity : 0l;
+            */
 
             m_mouseState.position += m_mouseState.velocity;
 
@@ -118,6 +141,20 @@ class UsbMouseComponent
             MOUSE_TURNING_OFF
         };
 
+        enum DirectionCommandEnum
+        {
+            DEG_NONE,
+            DEG_0,
+            DEG_45,
+            DEG_90,
+            DEG_135,
+            DEG_180,
+            DEG_225,
+            DEG_270,
+            DEG_315,
+            DEG_COUNT
+        };
+
         //typedef FixedPoint<int16_t, int16_t, 4> SignedFixedType;     // 12.4
         //typedef FixedPoint<int32_t, int32_t, 8> SignedFixedType;      // 24.8
         //typedef FixedPoint<int32_t, int64_t, 16> SignedFixedType;      // 16.16
@@ -133,14 +170,16 @@ class UsbMouseComponent
             int8_t      reportDeltaY;    ///< The motion Y component reported on USB.
 
             MouseStateType() :
-                reportDeltaX             (0),
-                reportDeltaY             (0)
+                reportDeltaX(0),
+                reportDeltaY(0)
             {
             }
         };
 
         unsigned long   m_lastUpdateTime;
         MouseStateType  m_mouseState;
+        //VectorType      m_mouseUnitVectors[DEG_COUNT];
+        VectorType      m_mouseUnitVectors[3][3];           // [row][col]
         MouseModeEnum   m_mouseMode;
 };
 
