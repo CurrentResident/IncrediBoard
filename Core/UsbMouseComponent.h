eb2ed1a5cbcf999c3bd48bee98064fdbf4d7270f
Jim Thoenen <jthoenen@gmail.com>
2019-01-02 08:26:36 -0600

    Initial refactored mouse motion

--- Core/UsbMouseComponent.h
+++ Core/UsbMouseComponent.h
@@ -14,20 +14,47 @@ class UsbMouseComponent
 
         UsbMouseComponent() :
             m_lastUpdateTime(Platform::GetMsec()),
-            m_mouseX        (0),
-            m_mouseY        (0),
-            m_mouseState    (MOUSE_OFF)
+            m_mouseMode     (MOUSE_OFF)
         {
         }
 
-        void MouseMotion(bool   i_upIsActive,
-                         bool   i_downIsActive,
-                         bool   i_leftIsActive,
-                         bool   i_rightIsActive,
-                         int8_t o_mouseX,
-                         int8_t o_mouseY)
+        void MouseMotion(bool    i_upIsActive,
+                         bool    i_downIsActive,
+                         bool    i_leftIsActive,
+                         bool    i_rightIsActive)
         {
-            const unsigned long now = Platform::GetMsec();
+            const unsigned long now       = Platform::GetMsec();
+            const int64_t       deltaMsec = static_cast<int64_t>(now - m_lastUpdateTime) << 16;
+
+            const int64_t deltaTSeconds  = (deltaMsec << 16) / (1000 << 16);    // The numerator needs another half-shift for FP-divide.
+
+            const int64_t MAX_VELOCITY = 1000 << 16;          // In units per second.
+
+            const int32_t actualVelocity = static_cast<int32_t>((MAX_VELOCITY * deltaTSeconds) >> 16);
+
+            m_mouseState.velocityX  = i_rightIsActive ? actualVelocity : 0;
+            m_mouseState.velocityX -= i_leftIsActive  ? actualVelocity : 0;
+
+            m_mouseState.velocityY  = i_downIsActive ? actualVelocity : 0;
+            m_mouseState.velocityY -= i_upIsActive   ? actualVelocity : 0;
+
+            m_mouseState.positionX += m_mouseState.velocityX;
+            m_mouseState.positionY += m_mouseState.velocityY;
+
+            const int32_t POINT_FIVE = 1 << 15;
+
+            m_mouseState.reportDeltaX = static_cast<int8_t>((m_mouseState.positionX - m_mouseState.positionXAtPreviousReport + POINT_FIVE) >> 16);
+            m_mouseState.reportDeltaY = static_cast<int8_t>((m_mouseState.positionY - m_mouseState.positionYAtPreviousReport + POINT_FIVE) >> 16);
+
+            if (m_mouseState.reportDeltaX != 0)
+            {
+                m_mouseState.positionXAtPreviousReport = m_mouseState.positionX;
+            }
+
+            if (m_mouseState.reportDeltaY != 0)
+            {
+                m_mouseState.positionYAtPreviousReport = m_mouseState.positionY;
+            }
 
             m_lastUpdateTime = now;
         }
@@ -35,17 +62,18 @@ class UsbMouseComponent
         void Process(BoardState& io_state)
         {
             // Mouse emulation
-            switch (m_mouseState)
+            switch (m_mouseMode)
             {
                 case MOUSE_ON:
                     io_state.m_keyReportArray.clear();
 
-                    m_mouseY = (io_state.m_activeKeyTable[IB_KEY_W] ? -2 :
-                               (io_state.m_activeKeyTable[IB_KEY_S] ?  2 : 0));
-                    m_mouseX = (io_state.m_activeKeyTable[IB_KEY_A] ? -2 :
-                               (io_state.m_activeKeyTable[IB_KEY_D] ?  2 : 0));
+                    MouseMotion(io_state.m_activeKeyTable[IB_KEY_W] != 0,
+                                io_state.m_activeKeyTable[IB_KEY_S] != 0,
+                                io_state.m_activeKeyTable[IB_KEY_A] != 0,
+                                io_state.m_activeKeyTable[IB_KEY_D] != 0);
 
-                    UsbInterface::MouseMove(m_mouseX, m_mouseY);
+                    UsbInterface::MouseMove(m_mouseState.reportDeltaX,
+                                            m_mouseState.reportDeltaY);
 
                     UsbInterface::MouseSetButtons(((io_state.m_activeKeyTable[IB_KEY_COMMA]  or io_state.m_activeKeyTable[IB_KEY_DELETE])    ? 1 : 0) |
                                                   ((io_state.m_activeKeyTable[IB_KEY_SLASH]  or io_state.m_activeKeyTable[IB_KEY_PAGE_DOWN]) ? 2 : 0) |
@@ -53,15 +81,15 @@ class UsbMouseComponent
 
                     if (not io_state.m_functionKey)
                     {
-                        m_mouseState = MOUSE_TURNING_OFF;
+                        m_mouseMode = MOUSE_TURNING_OFF;
                     }
                     break;
 
                 case MOUSE_TURNING_OFF:
-                    m_mouseState = MOUSE_OFF;
-                    m_mouseX     = 0;
-                    m_mouseY     = 0;
-                    UsbInterface::MouseMove(m_mouseX, m_mouseY);
+
+                    m_mouseMode = MOUSE_OFF;
+
+                    UsbInterface::MouseMove(0, 0);
                     UsbInterface::MouseSetButtons(0);
                     break;
 
@@ -70,7 +98,8 @@ class UsbMouseComponent
 
                     if (io_state.m_functionKey)
                     {
-                        m_mouseState     = MOUSE_ON;
+                        m_mouseState     = MouseStateType();
+                        m_mouseMode      = MOUSE_ON;
                         m_lastUpdateTime = Platform::GetMsec();
                     }
                     break;
@@ -79,20 +108,44 @@ class UsbMouseComponent
 
     private:
 
-        // Mouse state
+        // Mouse mode
         // ------------------------------------------------------------------------------
-        enum MouseStateEnum
+        enum MouseModeEnum
         {
             MOUSE_OFF,
             MOUSE_ON,
             MOUSE_TURNING_OFF
         };
 
-        unsigned long   m_lastUpdateTime;
-        uint8_t         m_mouseX;
-        uint8_t         m_mouseY;
-        MouseStateEnum  m_mouseState;
+        struct MouseStateType
+        {
+            int32_t positionX;      ///< Signed 16.16 fixed point.
+            int32_t positionY;      ///< Signed 16.16 fixed point.
+            int32_t velocityX;      ///< Signed 16.16 fixed point.
+            int32_t velocityY;      ///< Signed 16.16 fixed point.
 
+            int32_t positionXAtPreviousReport;
+            int32_t positionYAtPreviousReport;
+
+            int8_t reportDeltaX;    ///< The motion X component reported on USB.
+            int8_t reportDeltaY;    ///< The motion Y component reported on USB.
+
+            MouseStateType() :
+                positionX                (0),
+                positionY                (0),
+                velocityX                (0),
+                velocityY                (0),
+                positionXAtPreviousReport(0),
+                positionYAtPreviousReport(0),
+                reportDeltaX             (0),
+                reportDeltaY             (0)
+            {
+            }
+        };
+
+        unsigned long   m_lastUpdateTime;
+        MouseStateType  m_mouseState;
+        MouseModeEnum   m_mouseMode;
 };
 
 #endif
